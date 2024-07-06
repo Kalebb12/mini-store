c0ad3f309935a120d503e0d05ef2c7c3438c4429
Caleb <2021.cee.jay@gmail.com>
2024-07-06 08:00:14 +0100

    simple template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes ,Route} from 'react-router-dom'
 import './App.css'
-
+import Home from "./pages/home/home"
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/mini-store/" element={<Home/>}/>
+      </Routes>
     </div>
   )
 }

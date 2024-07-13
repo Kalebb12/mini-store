fb9e0b34c6477346f26daa015c459017a32ab7fd
Caleb <2021.cee.jay@gmail.com>
2024-07-13 11:36:25 +0100

    added banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,11 @@
+import './banner.css'
+const Banner = () => {
+  return (
+      <div className=" banner center padding">
+        <h1 className='title text-white'>Your No.1 plug for thrifts</h1>
+        <p className="secondary-text text-white">Purchase all kind of trifty wares here at affordable prices</p>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,10 @@
 import Footer from "../../components/footer/footer"
 import Products from "../../components/products/products"
-
+import Banner from "../../components/banner/banner"
 const Home = () => {
   return (
     <div >
+      <Banner/>
       <Products/>
       <Footer/>
     </div>
